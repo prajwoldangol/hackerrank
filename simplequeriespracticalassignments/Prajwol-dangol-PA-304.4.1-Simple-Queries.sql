76ec6060ee6e9c366d37c907c2030795cade11b6
prajwoldangol <prajwoldangol@gmail.com>
2024-04-10 14:36:03 -0700

    Practice Assignment 304.4.1 Simple Queries

---
+++ simplequeriespracticalassignments/Prajwol-dangol-PA-304.4.1-Simple-Queries.sql
@@ -0,0 +1,35 @@
+SELECT productName as "Name", 
+productLine as " Product Line", 
+buyPrice as " Buy Price"  
+FROM classicmodels.products 
+order by buyPrice DESC;
+
+
+SELECT contactFirstName as "First Name",
+contactLastName as "Last Name",
+city as "City"
+ FROM classicmodels.customers
+ where country = "Germany"
+ order by contactLastName ASC ;
+
+SELECT DISTINCT status
+FROM classicmodels.orders
+ORDER BY status ASC
+LIMIT 6;
+
+
+SELECT *
+FROM classicmodels.payments
+WHERE paymentDate >= '2005-01-01'
+ORDER BY paymentDate DESC;
+
+
+SELECT lastName,firstName,email,jobTitle 
+FROM classicmodels.employees
+WHERE officeCode = 1
+order by lastName;
+
+SELECT productName As Name, productLine As "Product Line", productScale As Scale, productVendor As Vendor
+FROM classicmodels.products
+WHERE productLine IN ('Classic Cars', 'Vintage Cars')
+ORDER BY productLine DESC, Name;
